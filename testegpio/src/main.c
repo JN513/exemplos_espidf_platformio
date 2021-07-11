18ec2027ee7593d35b3c75b477fcb95d979f1d98
Julio <jn5.guape@gmail.com>
2021-07-10 21:31:27 -0300

    Exemplo de uso da GPIO

---
+++ testegpio/src/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+
+const gpio_num_t led = GPIO_NUM_2;
+
+void setup(void);
+void loop(void);
+
+void app_main(void) {
+    setup();
+    loop();
+}
+
+void setup(){
+    printf("Exemplo GPIO\n");
+    gpio_pad_select_gpio(led);
+    gpio_set_direction(led, GPIO_MODE_OUTPUT);
+}
+
+void loop(){
+    bool state = 1;
+
+    while (1) {
+        state = !state;
+
+        printf("Piscando\n");
+        gpio_set_level(led, state);
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
